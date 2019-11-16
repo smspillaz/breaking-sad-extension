dcc1f1bb83846c31106b2a7afeda750eba49f7c1
Sam Spilsbury <smspillaz@gmail.com>
2019-11-17 00:32:11 +0200

    Setting: Make prettier

--- src/js/popup/Setting.jsx
+++ src/js/popup/Setting.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Input } from '@material-ui/core';
+import { Input, InputLabel } from '@material-ui/core';
 import { Button } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import FormControl from '@material-ui/core/FormControl';
 import { hot } from "react-hot-loader";
 import { Slider } from "material-ui-slider";
 // import styles from "./View.modules.css";
 import styled from 'styled-components'
+
+import icon from '../../img/icon-34.png';
+
 const Container = styled.div`
   width: 300px;
 `
+
+const Image = styled.img`
+  width: 50%;
+  margin-left: 25%;
+`;
+
+const LabelledInputField = ({ label, value, onChange }) => (
+  <FormControl onChange={onChange}>
+    <InputLabel>{label}</InputLabel>
+    <Input value={value}></Input>
+  </FormControl>
+);
+
+const SliderDiv = styled.div`
+  height: 50px;
+`;
+
 // unrealistic image, negative news, polarized content
 const Setting = ({
   onUnrealisticImagesSettingChange,
@@ -28,38 +53,37 @@ const Setting = ({
 }) => {
   return (
     <Container>
-      <p>HACK THE SAD WITH THE BAD ITS NOT A FACEBOOK AD</p>
-      <p>The World Is Full Of Sh*t Talkers—Don't Pay Them Any Attention</p>
-      {/* <img src={icon} /> */}
-      <div>
-        Unrealistic Images: {unrealisticImageSetting}
-        <Slider min={1} max={10} value={unrealisticImageSetting} onChange={onUnrealisticImagesSettingChange}/>
-      </div>
-      <div>
-        Negative News: {negativeNewsSetting}
-        <Slider min={1} max={10} value={negativeNewsSetting} onChange={onNegativeNewsSettingChange}/>
-      </div>
-      <div>
-        Polarized Content: {polarizedContentSetting}
-        <Slider min={1} max={10} value={polarizedContentSetting} onChange={onPolarizedContentSettingChange}/>
-      </div>
-      <div>
-        Filtered keywords (seperate by comma ","): 
-        <Input id="keyword" value={keywordsSetting} onChange={onKeywordSettingChange}/>
-      </div>
-      <div>
-        {'Depression '}
-        <Input id="depression" value={depressionSetting} onChange={e => onDepressionChange(e.target.value)}/>
-      </div>
-      <div>
-        {'Anxiety '}
-        <Input id="anxiety" value={anxietySetting} onChange={e => onAnxietyChange(e.target.value)}/>
-      </div>
-      <div>
-        {'Stress '}
-        <Input id="stress" value={stressSetting} onChange={e => onStressChange(e.target.value)}/>
-      </div>
-      <Button onClick={toggleShowQuestionnaire} >Questionnaire</Button>
+      <Card>
+        <CardMedia title={"Breaking Sad"}>
+          <Image src={icon} />
+        </CardMedia>
+        <CardContent style={{ paddingBottom: '0px' }}>
+          <SliderDiv>
+            Unrealistic Images: {unrealisticImageSetting}
+            <Slider min={1} max={10} value={unrealisticImageSetting} onChange={onUnrealisticImagesSettingChange}/>
+          </SliderDiv>
+          <SliderDiv>
+            Negative News: {negativeNewsSetting}
+            <Slider min={1} max={10} value={negativeNewsSetting} onChange={onNegativeNewsSettingChange}/>
+          </SliderDiv>
+          <SliderDiv>
+            Polarized Content: {polarizedContentSetting}
+            <Slider min={1} max={10} value={polarizedContentSetting} onChange={onPolarizedContentSettingChange}/>
+          </SliderDiv>
+          <LabelledInputField label={"Filtered keywords"} id="keyword" value={keywordsSetting} onChange={onKeywordSettingChange}/>
+          <LabelledInputField label={"Depression"} id="depression" value={depressionSetting} onChange={e => onDepressionChange(e.target.value)}/>
+          <LabelledInputField label={"Anxiety"} id="anxiety" value={anxietySetting} onChange={e => onAnxietyChange(e.target.value)}/>
+          <LabelledInputField label={"Stress"} id="stress" value={stressSetting} onChange={e => onStressChange(e.target.value)}/>
+        </CardContent>
+        <CardActions disableSpacing>
+          <Button
+            onClick={toggleShowQuestionnaire}
+            color="secondary"
+          >
+            Questionnaire
+          </Button>
+        </CardActions>
+      </Card>
     </Container>
   );
 };
